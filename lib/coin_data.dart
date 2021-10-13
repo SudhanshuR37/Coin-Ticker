7ee852cfb6bcfd838ca7ecfd4d0926be84920d5e
Sudhanshu Randive <s37randive@gmail.com>
2021-10-13 17:04:16 +0530

    Completed Project

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,5 +1,9 @@
 //TODO: Add your imports here.
 
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -31,8 +35,22 @@ const List<String> cryptoList = [
 ];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = 'YOUR-API-KEY-HERE';
+const apiKey = '409C446F-C20E-427D-83AF-431BB6020871';
 
 class CoinData {
   //TODO: Create your getCoinData() method here.
+  String url;
+
+  CoinData(this.url);
+
+  Future getCoinData() async {
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      return decodedData;
+    } else {
+      print(response.statusCode);
+    }
+  }
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
 import 'dart:io' show Platform;
 
+import 'coin_data.dart';
+
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
@@ -10,6 +12,10 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+  String rateBTC = '?';
+  String rateETH = '?';
+  String rateLTC = '?';
+  dynamic decodedData;
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -27,6 +33,9 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          getDataBTC();
+          getDataETH();
+          getDataLTC();
         });
       },
     );
@@ -50,10 +59,40 @@ class _PriceScreenState extends State<PriceScreen> {
 
   //TODO: Create a method here called getData() to get the coin data from coin_data.dart
 
+  void getDataBTC() async {
+    CoinData coinData = new CoinData(
+        '$coinAPIURL/${cryptoList[0]}/$selectedCurrency?apikey=$apiKey');
+    decodedData = await coinData.getCoinData();
+    setState(() {
+      rateBTC = decodedData['rate'].toString();
+    });
+  }
+
+  void getDataETH() async {
+    CoinData coinData = new CoinData(
+        '$coinAPIURL/${cryptoList[1]}/$selectedCurrency?apikey=$apiKey');
+    decodedData = await coinData.getCoinData();
+    setState(() {
+      rateETH = decodedData['rate'].toString();
+    });
+  }
+
+  void getDataLTC() async {
+    CoinData coinData = new CoinData(
+        '$coinAPIURL/${cryptoList[2]}/$selectedCurrency?apikey=$apiKey');
+    decodedData = await coinData.getCoinData();
+    setState(() {
+      rateLTC = decodedData['rate'].toString();
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     //TODO: Call getData() when the screen loads up.
+    getDataBTC();
+    getDataETH();
+    getDataLTC();
   }
 
   @override
@@ -64,28 +103,81 @@ class _PriceScreenState extends State<PriceScreen> {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  //TODO: Update the Text Widget with the live bitcoin data here.
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 28.0),
+                      child: Text(
+                        //TODO: Update the Text Widget with the live bitcoin data here.
+                        '1 ${cryptoList[0]} = $rateBTC $selectedCurrency',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 28.0),
+                      child: Text(
+                        //TODO: Update the Text Widget with the live bitcoin data here.
+                        '1 ${cryptoList[1]} = $rateETH $selectedCurrency',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 28.0),
+                      child: Text(
+                        //TODO: Update the Text Widget with the live bitcoin data here.
+                        '1 ${cryptoList[2]} = $rateLTC $selectedCurrency',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Container(
